26a508178752b349d2e988fcb477ae6857ed5ae6
Jefferson <jeffersonjitala11@gmail.com>
2018-08-14 19:02:30 -0500

    v1

--- js/actividad4.js
+++ js/actividad4.js
@@ -14,8 +14,13 @@ function actividad4(){
     var pre2 = document.getElementById('pre2').value;
     var pre3 = document.getElementById('pre3').value;
     var pre4 = document.getElementById('pre4').value;
-
-    if(pre1 =="Manolo" || pre1 =="vaca" || pre1=="Mercedes")
+    if(cor < 1 && inc< 1 && pre1 =="" && pre2 =="" && pre3=="" )
+    {
+        alert("Debe escoger al menos 1 respuesta a la pregunta");
+    }
+    else
+    {
+        if(pre1 =="Manolo" || pre1 =="vaca" || pre1=="Mercedes")
     {
         if(pre1!=pre2 && pre1!=pre3)
         {
@@ -29,6 +34,12 @@ function actividad4(){
             $("#pre1").addClass('incorrecto')
         }
     }
+    else
+        {
+            inc=inc+1;
+            $("#pre1").removeClass('correcto')
+            $("#pre1").addClass('incorrecto')
+        }
 
     if(pre2 =="Manolo" || pre2 =="vaca" || pre2=="Mercedes")
     {
@@ -44,6 +55,12 @@ function actividad4(){
             $("#pre2").addClass('incorrecto')
         } 
     }
+    else
+        {
+            inc=inc+1;
+            $("#pre2").removeClass('correcto')
+            $("#pre2").addClass('incorrecto')
+        }
     if(pre3 =="Manolo" || pre3 =="vaca" || pre3=="Mercedes")
     {
         if(pre3!=pre1 && pre3!=pre2)
@@ -58,65 +75,98 @@ function actividad4(){
             $("#pre3").addClass('incorrecto')
         } 
     }
-
-    if(pre4 =="Martina" )
+    else
+        {
+            inc=inc+1;
+            $("#pre3").removeClass('correcto')
+            $("#pre3").addClass('incorrecto')
+        }
+    if(pre4!="")
     {
-            cor=cor+1;
-            $("#pre4").addClass('correcto')
+        if(pre4 =="Martina" )
+        {
+                cor=cor+1;
+                $("#pre4").addClass('correcto')
+        }
+        else
+        {
+                inc=inc+1;
+                $("#pre4").removeClass('correcto')
+                $("#pre4").addClass('incorrecto')
+        }
     }
     else
     {
-            inc=inc+1;
-            $("#pre4").removeClass('correcto')
-            $("#pre4").addClass('incorrecto')
-    
+        inc=inc+1;
+                $("#pre4").removeClass('correcto')
+                $("#pre4").addClass('incorrecto')
     }
 
     var com=document.getElementById("cbovaca").value;
-    if(com=="3")
+    if(com!="1")
     {
-        cor=cor+1;
-        $("#cbovaca").addClass('correcto')
-        
+        if(com=="3")
+        {
+            cor=cor+1;
+            $("#cbovaca").addClass('correcto')
+
+        }
+        else
+        {
+            inc=inc+1;
+            $("#cbovaca").removeClass('correcto')
+            $("#cbovaca").addClass('incorrecto')
+        }
     }
     else
     {
         inc=inc+1;
-        $("#cbovaca").removeClass('correcto')
-        $("#cbovaca").addClass('incorrecto')
+            $("#cbovaca").removeClass('correcto')
+            $("#cbovaca").addClass('incorrecto')
     }
     var pre5=document.getElementById("pre5").value;
-    if(pre5=="manzanas" || pre5=="manzana")
+    if(pre5!="")
     {
-        cor=cor+1;
-        $("#pre5").addClass('correcto')
+        if(pre5=="manzanas" || pre5=="manzana")
+        {
+            cor=cor+1;
+            $("#pre5").addClass('correcto')
+        }
+        else
+        {
+            inc=inc+1;
+            $("#pre5").removeClass('correcto')
+            $("#pre5").addClass('incorrecto')
+        }
     }
     else
     {
         inc=inc+1;
-        $("#pre5").removeClass('correcto')
-        $("#pre5").addClass('incorrecto')
+            $("#pre5").removeClass('correcto')
+            $("#pre5").addClass('incorrecto')
     }
 
     var pre6=document.getElementById("pre6").value;
-    if(pre6=="muuuu")
+    if(pre6!="")
     {
-        cor=cor+1;
-        $("#pre6").addClass('correcto')
+        if(pre6=="muuuu")
+        {
+            cor=cor+1;
+            $("#pre6").addClass('correcto')
+        }
+        else
+        {
+            inc=inc+1;
+            $("#pre6").removeClass('correcto')
+            $("#pre6").addClass('incorrecto')
+        }
     }
     else
     {
         inc=inc+1;
         $("#pre6").removeClass('correcto')
-        $("#pre6").addClass('incorrecto')
+        $("#pre6").addClass('incorrecto')   
     }
-
-
-
-    if(cor < 1 && inc< 1 ){
-        alert("Debe escoger al menos 1 respuesta a la pregunta");
-        
-    }else{
             var NotaF = ((cor-inc) * 10) / 7;
             if( NotaF<0)
             {
@@ -129,7 +179,7 @@ function actividad4(){
                 $('#txtNota').html(NotaF.toFixed(2) + ' ');
                 document.getElementById('bt_comprobar').disabled = true;
             }
-        }
     }
+}
 
   
